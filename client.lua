64d1cd78c7213bd3afd80c5cf1d067112edf807a
Eric Santos <ericsantosbr2@gmail.com>
2018-11-13 01:44:56 -0200

    updated client file for routing tests

--- client.lua
+++ client.lua
@@ -1,20 +1,40 @@
 local socket = require "socket"
 local json = require "json"
 
+print("Type the requirement that you want to do")
+local requirement = io.read()
+
+-- starts a connection to the server
 local server = socket.connect("localhost", 80324)
 
+-- table with the requirement
 local reqtab = {
-	requirement = "teste"
+	requirement = requirement
 }
 
-local msg = json.encode(reqtab) .. "\n"
+if server ~= nil then
+	-- encodes the requirement to a json format
+	local msg = json.encode(reqtab) .. "\n"
 
-io.write(msg)
+	-- sends the requirement
+	server:send(msg)
 
-server:send(msg)
+	-- gets a response
+	response = server:receive()
+	response = json.decode(response)
 
-response = server:receive()
+	-- treats the response
+	if response.msg == "error" then
+		print("error! ", response.err)
+	else
+		if response.requirement == "hour" then
+			io.write(response.hour, ":", response.min, ":", response.sec, "\n")
+		elseif response.requirement == "test" then
+			print(response.msg)
+		end
+	end
 
-print(response)
-
-server:close()
+	server:close()
+else
+	print("error, can't reach the server")
+end
