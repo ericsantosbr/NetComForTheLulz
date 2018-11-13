96d6e80b09e612233bc935b04985bdbac530f930
Eric Santos <ericsantosbr2@gmail.com>
2018-11-13 01:45:20 -0200

    updated router file for more routing options

--- router.lua
+++ router.lua
@@ -2,12 +2,39 @@ local json = require "json"
 
 local router = {}
 
+-- main routement to treat the messages
 function routement(msg)
-	if msg == "teste" then return "success!"
-	else return "error" end
+	local response = {}
+	response.requirement = msg
+	if msg == "test" then
+		response.msg = "success"
+
+		response = json.encode(response)
+
+		return response
+	elseif msg == "hour" then
+		local time = os.date("*t")
+
+		response.msg = "success"
+		response.hour = time.hour
+		response.min = time.min
+		response.sec = time.sec
+
+		response = json.encode(response)
+
+		return response
+	else
+		response.msg = "error"
+		response.err = "routment not found"
+
+		response = json.encode(response)
+
+		return response
+	end
 
 end
 
+-- function that starts the treatment
 function router.treatment(msg)
 	totreat = json.decode(msg)
 	returnmsg = ""
